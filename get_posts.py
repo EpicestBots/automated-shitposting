7d0f15e54f8116787adba8553c9121790bab9d85
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-05-28 20:50:36 +0530

    fix: preview file selector

--- get_posts.py
+++ get_posts.py
@@ -5,6 +5,8 @@ import requests
 
 load_dotenv()
 
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
 
 def convert_bytes(num):
     for x in ["bytes", "KB", "MB", "GB", "TB"]:
@@ -30,10 +32,21 @@ def get_top_post(subreddit) -> dict:
         opts = {}
         if (not submission.over_18 and not submission.pinned and not submission.is_video):
             file_size = get_file_size(submission.url)
-            if file_size > 5 * 1024 * 1024:
-                preview_file = submission.preview['images'][0]['source']['url']
+            if file_size > MAX_FILE_SIZE:
+                preview_base = submission.preview['images'][0]['variants']['gif']
+                preview_file = preview_base['source']['url']
                 print(
-                    f"File too large ({convert_bytes(file_size)}), using preview instead with size {convert_bytes(get_file_size(preview_file))}")
+                    f"File too large ({convert_bytes(file_size)}). Checking previews with type gif:")
+                for preview in preview_base['resolutions']:
+                    preview_size = get_file_size(preview['url'])
+                    if preview_size < MAX_FILE_SIZE:
+                        preview_file = preview['url']
+                        print(
+                            f"Size: {convert_bytes(preview_size)}")
+                    else:
+                        break
+                print(
+                    f"Using preview instead with size {convert_bytes(get_file_size(preview_file))}\n")
                 opts["url"] = preview_file
             else:
                 opts["url"] = submission.url
